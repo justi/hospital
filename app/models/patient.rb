c6e5c3a09c740148f0f4615b75a7ed72e8460e7a
Justyna Wojtczak <justine84@gmail.com>
2019-03-30 12:32:48 +0100

    Add validations to models

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,5 @@
 class Patient < ApplicationRecord
+  validates_presence_of :name, :surname, :pesel
+  validates :pesel, format: { with: /\d{11}/,
+                              message: "should contain 11 digits"}
 end
--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,2 +1,9 @@
 class Staff < ApplicationRecord
+  validates_presence_of :name, :surname, :pesel
+  validates :pesel, format: { with: /\d{11}/,
+                              message: "should contain 11 digits"}
+  validates :occupation, inclusion: { in: %w(doctor nurse),
+                                      message: "should be doctor or nurse"}
+
+  enum occupation: { doctor: 0, nurse: 1 }
 end
