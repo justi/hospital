ffbbeb418320b0580ad96e227940e32ce8383fe2
Justyna Wojtczak <justine84@gmail.com>
2019-03-30 11:36:49 +0100

    Generate scaffold for Patient

---
+++ test/system/patients_test.rb
@@ -0,0 +1,47 @@
+require "application_system_test_case"
+
+class PatientsTest < ApplicationSystemTestCase
+  setup do
+    @patient = patients(:one)
+  end
+
+  test "visiting the index" do
+    visit patients_url
+    assert_selector "h1", text: "Patients"
+  end
+
+  test "creating a Patient" do
+    visit patients_url
+    click_on "New Patient"
+
+    fill_in "Name", with: @patient.name
+    fill_in "Pesel", with: @patient.pesel
+    fill_in "Surname", with: @patient.surname
+    click_on "Create Patient"
+
+    assert_text "Patient was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Patient" do
+    visit patients_url
+    click_on "Edit", match: :first
+
+    fill_in "Name", with: @patient.name
+    fill_in "Pesel", with: @patient.pesel
+    fill_in "Surname", with: @patient.surname
+    click_on "Update Patient"
+
+    assert_text "Patient was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Patient" do
+    visit patients_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Patient was successfully destroyed"
+  end
+end
