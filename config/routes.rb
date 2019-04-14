027419e6e3b77ff9290dc2a1845c931c7eff64a2
Justyna Wojtczak <justine84@gmail.com>
2019-04-14 12:31:11 +0200

    Update devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login' }
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+  end
   root to: "staffs#index"
   resources :bills
   resources :bill_items
