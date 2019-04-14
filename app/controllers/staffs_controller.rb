1b380c314659d4a11855dbff3dc71b8d116318af
Justyna Wojtczak <justine84@gmail.com>
2019-04-14 12:58:57 +0200

    Staff authorization

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -1,4 +1,5 @@
 class StaffsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_staff, only: [:show, :edit, :update, :destroy]
 
   # GET /staffs
